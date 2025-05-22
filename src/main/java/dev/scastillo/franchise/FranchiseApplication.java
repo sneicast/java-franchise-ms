3974e5bc772c5208f771cda4b65fde08aaac58e7
Sneider Castillo <sneiderz15@gmail.com>
2025-05-21 19:34:21 -0500

    commit init

---
+++ src/main/java/dev/scastillo/franchise/FranchiseApplication.java
@@ -0,0 +1,13 @@
+package dev.scastillo.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseApplication.class, args);
+	}
+
+}
